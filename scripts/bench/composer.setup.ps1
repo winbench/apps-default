e2fdb8482b01c448a3e71207181c23229ee5d542
Tobias Kiertscher <kiertscher@th-brandenburg.de>
2017-03-29 13:14:26 +0200

    improved setup script for Composer

--- scripts/bench/composer.setup.ps1
+++ scripts/bench/composer.setup.ps1
@@ -1,9 +1,14 @@
+$php5 = App-Exe "Bench.PHP5"
+$php7 = App-Exe "Bench.PHP7"
+if ($php7) { $php = $php7 } else { $php = $php5 }
+$wget = App-Exe "Bench.Wget"
+
 $composerDir = App-Dir "Bench.Composer"
 if (!(Test-Path $composerDir)) { $_ = mkdir $composerDir }
 cd $composerDir
 
-$expectedSignature = $(wget -q -O - https://composer.github.io/installer.sig)
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+$expectedSignature = $(& $wget -q -O - https://composer.github.io/installer.sig)
+& $wget -q -O "composer-setup.php" "https://getcomposer.org/installer"
 $actualSignature = $(php -r "echo hash_file('SHA384', 'composer-setup.php');")
 
 if ( $expectedSignature -ne $actualSignature )
@@ -15,7 +20,7 @@ if ( $expectedSignature -ne $actualSignature )
 
 "@php `"%~dp0composer.phar`" %*" | Out-File "composer.bat" -Encoding Default
 
-php composer-setup.php --quiet
+& $php composer-setup.php --quiet
 $result = $LastExitCode
 del composer-setup.php
 exit $result
