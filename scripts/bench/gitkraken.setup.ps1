4a30a4e7656b5eb4dca7d2ad6f0b4637afc6c771
Tobias Kiertscher <dev@mastersign.de>
2018-06-24 16:35:42 +0200

    fixed setup script for GitKraken

--- scripts/bench/gitkraken.setup.ps1
+++ scripts/bench/gitkraken.setup.ps1
@@ -1,12 +1,9 @@
 $configDir = [IO.Path]::Combine((Get-ConfigValue "AppDataDir"), ".gitkraken")
+$profilesDir = [IO.Path]::Combine($configDir, "profiles")
+
+if (!(Test-Path $configDir)) { mkdir $configDir | Out-Null }
+if (!(Test-Path $profilesDir)) { mkdir $profilesDir | Out-Null }
+
 $configFile = [IO.Path]::Combine($configDir, "config")
 $templateFile = App-SetupResource "Bench.GitKraken" "config"
-
-if (!(Test-Path $configDir))
-{
-    $_ = mkdir $configDir
-}
-if (!(Test-Path $configFile))
-{
-    copy $templateFile $configFile
-}
+if (!(Test-Path $configFile)) { copy $templateFile $configFile }
